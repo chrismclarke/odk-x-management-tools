c72a7e58aa23a2d0780cb9f2d29dce6008b27d54
chrismclarke <chris.m.clarke@live.co.uk>
2020-05-20 12:30:51 -0700

    code tidying

--- apps/dashboard/src/app/services/export.service.ts
+++ apps/dashboard/src/app/services/export.service.ts
@@ -4,12 +4,7 @@ import { saveAs } from 'file-saver';
 
 @Injectable({ providedIn: 'root' })
 export class ExportService {
-  constructor() {
-    console.log('papa', Papa);
-  }
-
   exportToCSV(data: any, filename: string) {
-    console.log('exporting to csv', data);
     const csvData = Papa.unparse(data);
     const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
     return saveAs(blob, filename);
