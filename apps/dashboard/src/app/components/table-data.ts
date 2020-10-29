b5fa87b9d7e269b678dbcd8982b4ce9d1d8e2f95
Chris <chris.m.clarke@live.co.uk>
2020-10-29 12:55:58 +0000

    table style improvements

--- apps/dashboard/src/app/components/table-data.ts
+++ apps/dashboard/src/app/components/table-data.ts
@@ -7,27 +7,33 @@ import { OdkRestService } from '../services/odkrest.service';
 @Component({
   selector: 'odkxm-table-data',
   template: `
-    <div class="container">
-      <table mat-table [dataSource]="dataSource" class="mat-elevation-z8">
-        <ng-container *ngFor="let col of displayedColumns" [matColumnDef]="col">
-          <th mat-header-cell *matHeaderCellDef>{{ col }}</th>
-          <td mat-cell *matCellDef="let data">{{ data[col] }}</td>
-        </ng-container>
-        <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-        <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
-      </table>
-
+    <mat-card>
+      <div class="container">
+        <table mat-table [dataSource]="dataSource">
+          <ng-container
+            *ngFor="let col of displayedColumns"
+            [matColumnDef]="col"
+          >
+            <th mat-header-cell *matHeaderCellDef>{{ col }}</th>
+            <td mat-cell *matCellDef="let data">{{ data[col] }}</td>
+          </ng-container>
+          <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+          <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
+        </table>
+      </div>
       <mat-paginator
+        style="margin-top:1em"
         [pageSizeOptions]="[5, 10, 50]"
         showFirstLastButtons
       ></mat-paginator>
-    </div>
+    </mat-card>
   `,
   styles: [
     `
       .container {
         width: 100%;
         overflow-x: auto;
+        padding-bottom: 1em;
       }
       table {
         width: 100%;
@@ -36,8 +42,24 @@ import { OdkRestService } from '../services/odkrest.service';
       td.mat-cell {
         padding: 5px;
       }
-    `
-  ]
+      td,
+      th {
+        width: 100px;
+        min-width: 70px;
+        max-width: 130px;
+      }
+      td {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      th {
+        word-break: break-all;
+        vertical-align: baseline;
+        overflow: auto;
+      }
+    `,
+  ],
 })
 export class TableDataComponent {
   dataSource: MatTableDataSource<ITableRow>;
@@ -61,7 +83,7 @@ export class TableDataComponent {
   private _sortColumns(keys: string[]) {
     const metaKeys = [];
     const valKeys = [];
-    keys.forEach(k => {
+    keys.forEach((k) => {
       if (k.charAt(0) === '_') metaKeys.push(k);
       else valKeys.push(k);
     });
