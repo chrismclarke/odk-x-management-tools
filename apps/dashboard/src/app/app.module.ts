f21294a822e3bc559edc2415b15680fc79f3be8f
Chris <chris.m.clarke@live.co.uk>
2020-10-30 16:32:01 +0000

    remove deprecated http client

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -2,14 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {
-  OdkTablesInterceptor,
-  ResponseLoggerInterceptor,
-} from './interceptors/';
 import { ComponentsModule } from './components/components.module';
 
 import { ConnectionComponent, TablesComponent, ExportComponent } from './pages';
@@ -23,25 +18,13 @@ import { ConnectionComponent, TablesComponent, ExportComponent } from './pages';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
     ComponentsModule,
     AppRoutingModule,
   ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: OdkTablesInterceptor,
-      multi: true,
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ResponseLoggerInterceptor,
-      multi: true,
-    },
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
