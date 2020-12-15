6ed694031b895dcac417761d13e7dc2f5b5a9f92
chrismclarke <chris.m.clarke@live.co.uk>
2020-12-14 23:02:25 -0800

    add default config

--- apps/dashboard/src/environments/environment.prod.ts
+++ apps/dashboard/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
+import DASHBOARD_CONFIG from './config.json';
+
 export const environment = {
   production: true,
-  appVersion: require('../../../../package.json').version
+  appVersion: require('../../../../package.json').version,
+  DASHBOARD_CONFIG,
 };
--- apps/dashboard/src/environments/environment.ts
+++ apps/dashboard/src/environments/environment.ts
@@ -1,10 +1,12 @@
+import DASHBOARD_CONFIG from './config.json';
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  appVersion: require('../../../../package.json').version
+  appVersion: require('../../../../package.json').version,
+  DASHBOARD_CONFIG,
 };
 
 /*
